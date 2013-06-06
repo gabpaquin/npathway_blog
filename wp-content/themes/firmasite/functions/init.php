65e7a1de6ea99aa730d8a845bb01ce16bad85008
Stephan Fuchs <sfuchs@wfuchs.net>
2013-06-06 18:32:52 +0800

    fixed parent theme style issue

--- wp-content/themes/firmasite/functions/init.php
+++ wp-content/themes/firmasite/functions/init.php
@@ -147,7 +147,7 @@ function firmasite_enqueue_script() {
 		wp_enqueue_style( 'bootstrap-responsive' );
 	}
 	// style
-	wp_register_style( 'style', get_template_directory_uri() . '/style.css' );
+	wp_register_style( 'style', get_stylesheet_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'style' );
 	
 	// bootstrap-js
